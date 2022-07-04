f9192b7a07e031215175b4a51a0bd502e5e24baa
9berny9 <bernatik.michal@gmail.com>
2022-07-04 15:18:35 +0200

    ready for finishing

--- result.py
+++ result.py
@@ -7,13 +7,42 @@ def ProductIteration(*args, repeat):
     return result
 
 
-def TwosPair(list_1):
-    for curr_value, next_value in zip(list_1, list_1[1:]):
+def TwosPair(iteration):
+    for curr_value, next_value in zip(iteration, iteration[1:]):
         if curr_value == 1 and next_value == 1:
             return True
 
 
-test = [155, 55, 2, 55, 43, 87]
+def FindValues(without_turbulenc, passengers):
+    posibilities = []
+    for b in without_turbulenc:
+        values = []
+        for i, j in enumerate(b):
+            if j == 1:
+                values.append(passengers[i])
+        posibilities.append(values)
+    return posibilities
+
+
+def maximumSum(list1):
+    maxi = 0
+
+    # traversal in the lists
+    for x in list1:
+        sum = 0
+        # traversal in list of lists
+        for y in x:
+            sum += y
+        maxi = max(sum, maxi)
+
+    return maxi
+
+
+
+test = [463,  73, 282, 422, 271, 118, 112]
 
 all_combinations = ProductIteration(range(2), repeat=len(test))
 without_turbulence = [i for i in all_combinations if not TwosPair(i)]
+all_possibilities = FindValues(without_turbulence, test)
+result = maximumSum(all_possibilities)
+print(result)
